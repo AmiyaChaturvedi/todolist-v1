2ea7c6bb53a14d3c8e71bbf2651c605ee2c7fb29
AmiyaChaturvedi <amiyachaturvedi2871@gmail.com>
2021-07-30 05:13:10 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+let items = ["Buy Food.", "Cook Food.", "Eat Food."];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+    let day = date.getDate();
+
+    res.render("list", {
+        listTitle: day,
+        newListItems: items
+    });
+});
+
+app.post("/", function(req, res) {
+    let item = req.body.newItem;
+    console.log(req.body);
+    if (req.body.list === "Work List") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+app.get("/work", function(req, res) {
+
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });
+});
+
+
+app.post("/work", function(req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
+app.get("/about", function(req, res) {
+    res.render("about");
+});
+
+
+app.listen(3000, function() {
+    console.log("Server is up and running on port 3000");
+})
+
+
+// switch (currentDay) {
+//     case 0:
+//         day = "Sunday";
+//         break;
+
+//     case 1:
+//         day = "Monday";
+//         break;
+
+//     case 2:
+//         day = "Tuesday";
+//         break;
+
+//     case 3:
+//         day = "Wednesday";
+//         break;
+
+//     case 4:
+//         day = "Thursday";
+//         break;
+
+//     case 5:
+//         day = "Friday";
+//         break;
+
+//     case 6:
+//         day = "Saturday";
+//         break;
+
+//     default:
+//         console.log("Error: Current day is equal to:" + currentDay);
+// }
